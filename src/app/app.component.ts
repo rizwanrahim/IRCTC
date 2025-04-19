b4a704b300bf2efcfd9c558d10087ef85c12ff9b
Ishra <ishraali2000@gmail.com>
2025-04-19 15:52:35 +0530

    add nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
