ed6a2282826944b289f6ba1d36a27fc5aaaee5ff
Rizwan Rahim <rizwan.mfrahim@outlook.com>
2025-04-20 13:35:46 +0530

    Add material theme and load icon from local

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { AppConstants } from '../app-constant';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'nav-menu',
-  imports: [],
+  imports: [MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
 })
