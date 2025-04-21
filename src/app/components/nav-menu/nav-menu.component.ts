3d34d4a85e1510d6f7f79f29007e1ddd6e482ca2
Rizwan Rahim <rizwan.mfrahim@outlook.com>
2025-04-21 19:39:45 +0530

    Add side nav

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import { AppConstants } from '../app-constant';
+import { Component, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { SideNavComponent as SideNavComponent } from '../side-nav/side-nav.component';
 @Component({
   selector: 'nav-menu',
   imports: [MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule],
+    MatMenuModule, SideNavComponent],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
 })
 export class NavMenuComponent {
-  readonly logoPath = AppConstants.LOGO_PATH;
+  @ViewChild(SideNavComponent) sidenavComponent!: SideNavComponent;
+
+  toggleSidenav() {
+    this.sidenavComponent.toggle();
+  }
+
 }
