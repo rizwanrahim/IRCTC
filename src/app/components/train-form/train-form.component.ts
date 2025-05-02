237caa212ef788d4e6e3a9a60f20b3561136f937
Rizwan Rahim <rizwanrahim@users.noreply.github.com>
2025-05-02 17:44:06 +0530

    Add train form view

--- src/app/components/train-form/train-form.component.ts
+++ src/app/components/train-form/train-form.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'train-form',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './train-form.component.html',
   styleUrl: './train-form.component.css'
 })
 export class TrainFormComponent {
+  trainForm: FormGroup;
+  submitted: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.trainForm = this.fb.group({
+      trainName: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.trainForm.valid) {
+      const trainData = this.trainForm.value;
+      console.log('Train Data:', trainData);
+      this.trainForm.reset();
+      this.trainForm.markAsPristine();
+      this.trainForm.markAsUntouched();
+      this.submitted = true;  
+    }
+  }
 }
